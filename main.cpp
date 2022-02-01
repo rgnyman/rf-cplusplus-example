2a51b61d82e43005d29ed840f4be2d7f74f03199
Ran Nyman <ran@gosei.fi>
2022-02-01 10:43:58 +0200

    Added auto and delete

--- main.cpp
+++ main.cpp
@@ -9,15 +9,17 @@
 
 int main(int arc, char **argv) {
     MovieDB::CustomerDb customerDb = MovieDB::CustomerDb::getInstance();
-    MovieDB::Customer *customerRentals = new MovieDB::Customer("Sally");
-    MovieDB::Movie *movie1 = new MovieDB::Movie("Gone with the Wind", new MovieDB::Regular{});
-    MovieDB::Rental *rental1 = new MovieDB::Rental(*movie1, 3); // 3 day rental
+    auto *customerRentals = new MovieDB::Customer("Sally");
+    auto *movie1 = new MovieDB::Movie("Gone with the Wind", new MovieDB::Regular{});
+    auto *rental1 = new MovieDB::Rental(*movie1, 3); // 3 day rental
     customerRentals->addRental(*rental1);
     customerRentals->getNumberRentals();
     std::cout << customerRentals->statement() << std::endl;
     customerDb.addCustomerRentals(*customerRentals);
-    
-//    MovieDB::SimpleUi ui{};
-//    ui.drawUi();
+    customerDb.store();
+
+    delete rental1;
+    delete movie1;
+    delete customerRentals;
 
 }
